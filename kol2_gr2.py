0f5b06d61f65b9acd0227f2b87007081e8a17b87
pgruszczynski93 <pgruszczynski93@gmail.com>
2016-11-09 14:27:32 +0100

    Update kol2_gr2.py

--- kol2_gr2.py
+++ kol2_gr2.py
@@ -14,3 +14,29 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+from Student import *
+
+def main():
+	stud = Student("Ala","Makota",[2,3,4,5],[1,1,0])
+	stud.get_scores()
+	print("\nSrednia: ")
+	stud.avg_total()
+	stud.get_attendances()
+
+	stud = Student("KAla","VaMakota",[2,3,4,5,4,5,4,2],[1,1,0,1,1])
+	stud.get_scores()
+	print("\nSrednia: ")
+	stud.avg_total()
+	stud.get_attendances()
+
+	
+	stud = Student("AsAla","Sama",[2,3,4,5,2,3,4,5,4,2,2,1,1,4],[1,1,0])
+	stud.get_scores()
+	print("\nSrednia: ")
+	stud.avg_total()
+	stud.get_attendances()
+
+	
+if __name__ == "__main__":
+	main()
