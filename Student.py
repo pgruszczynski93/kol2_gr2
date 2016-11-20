9fb530a5000d4cc91d7cb985a622c8821c979176
pgruszczynski93 <pgruszczynski93@gmail.com>
2016-11-20 22:50:53 +0100

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,33 +1,12 @@
 class Student(object):
-	def __init__(self,name,surname,scores,attendances):
-		self.name = name
-		self.surname = surname
-		self.scores = scores
-		self.attendances = attendances
-		
+	def __init__(self,stud_descr, subject_count):
+		self.stud_descr = stud_descr.split(' ')
+		self.name = self.stud_descr[0]
+		self.surname = self.stud_descr[1]
+		self.subject_count = subject_count
 
 	def get_name(self):
 		return self.name
 
 	def get_surname(self):
 		return self.surname
-
-	def show_student(self):
-		print("Imie %s, Nazwisko %s"%(self.get_name(), self.get_name()))
-
-	def get_scores(self):
-		print("Oceny studenta z przedmiotow %s %s"%(self.get_name(),self.get_surname()))
-		for score in self.scores:
-			print (score),
-
-	def get_attendances(self):
-		print("Obecnosc studenta %s %s"%(self.get_name(),self.get_surname()))
-		for attendance in self.attendances:
-			print (attendance),
-	
-	def avg_total(self):
-		print sum(self.scores)/len(self.scores)
-
-	
-	def avg_subject(self):
-		pass
