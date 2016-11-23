4dc4889d03bf7adb6cad0ca762816f1d17ca0618
pgruszczynski93 <pgruszczynski93@gmail.com>
2016-11-23 11:50:23 +0100

    Update Student.py

--- Student.py
+++ Student.py
@@ -10,3 +10,36 @@ class Student(object):
 
 	def get_surname(self):
 		return self.surname
+
+
+class Subject(object):
+	def __init__(self,sub_name):
+		self.sub_name = sub_name
+
+	def get_sub_name(self):
+		return self.sub_name
+
+
+class StudentData(object):
+	def __init__(self, grades, attendances):
+		self.grades = grades
+		self.attendances = attendances
+		self.total_avg = sum(sum(i)/8 for i in zip(*self.grades))/6
+
+	def get_grades(self):
+		return self.grades
+
+	def get_grade(self,index):
+		return self.grades[index]
+
+	def get_attendances(self):
+		return self.attendances
+
+	def get_attendance(self, index):
+		return self.attendances[index]
+
+	def get_subject_average(self):
+		return sum(self.grades)/len(self.grades)
+
+	def get_total_avg(self):
+		return self.total_avg
